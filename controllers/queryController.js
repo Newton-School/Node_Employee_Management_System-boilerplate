80e5612bbda0258c892cce947f3f50594b07b479
MannShah17 <mannshah2004@gmail.com>
2023-04-28 18:48:58 +0530

    done

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -2,24 +2,10 @@ const Employee = require('../models/employeeModel');
 
 const filterQueries = async (req, res) => {
   try {
-    const { firstName, lastName } = req.query || '';
-    const sort = req.query.sort || 'salary';
-    const page = parseInt(req.query.page) || 1; // Default page is 1
-    const limit = parseInt(req.query.limit) || 5; // Default limit is 5
-    const queryObject = {};
-
-    if (firstName) {
-      queryObject.firstName = { $regex: firstName, $options: 'i' };
-    }
-    if (lastName) {
-      queryObject.lastName = { $regex: lastName, $options: 'i' };
-    }
-    const employee = await Employee.find(queryObject)
-      .sort(sort.startsWith('-') ? sort.substring(1) : sort)
-      .skip((page - 1) * limit) // Skip records for pagination
-      .limit(limit); // Limit the number of records returned for pagination
-
-    res.status(200).json(employee);
+    //Write your code here for sorting, pagination, & searching
+    //1) For sorting sort salary from ascending to descending order
+    //2) For Pagination set limit 5 as a default limit and default page is 1
+    //3) For searching search for firstName and lastName with caseinsensitive
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
