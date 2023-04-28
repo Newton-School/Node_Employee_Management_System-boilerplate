54ce4b6eb45fd687848095c1ce330f1e7a99bf49
MannShah17 <mannshah2004@gmail.com>
2023-04-28 23:52:36 +0530

    done

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -5,7 +5,6 @@ const filterQueries = async (req, res) => {
     //Write your code here for sorting, pagination, & searching
     //1) For sorting sort salary from ascending to descending order
     //2) For Pagination set limit 5 as a default limit and default page is 1
-    //3) For searching search for firstName and lastName with caseinsensitive
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
