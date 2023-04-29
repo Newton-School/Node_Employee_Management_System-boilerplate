ac233ceb57e4d1426e7abad8e3d87a5344bcd0a4
MannShah17 <mannshah2004@gmail.com>
2023-04-29 11:33:53 +0530

    done

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -2,7 +2,7 @@ const Employee = require('../models/employeeModel');
 
 const filterQueries = async (req, res) => {
   try {
-    //Write your code here for sorting, pagination, & searching
+    //Write your code here for sorting & pagination
     //1) For sorting sort salary from ascending to descending order
     //2) For Pagination set limit 5 as a default limit and default page is 1
   } catch (err) {
